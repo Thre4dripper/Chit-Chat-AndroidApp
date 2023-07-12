921f626110c87e786e763d6861ba20d3ca533837
Ijlal Ahmad <ijlalahmad845@gmail.com>
2023-07-12 10:25:21 +0530

    designed home screen layout

--- app/src/main/java/com/example/chitchatapp/HomeActivity.kt
+++ app/src/main/java/com/example/chitchatapp/HomeActivity.kt
@@ -14,6 +14,8 @@ class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
 
     private val auth = FirebaseAuth.getInstance()
+
+    //navigation drawer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
@@ -23,7 +25,7 @@ class HomeActivity : AppCompatActivity() {
         if (auth.currentUser == null)
             signInLauncher.launch(Auth.googleSignIn())
 
-        binding.btnLogout.setOnClickListener {
+        binding.logoutBtn.setOnClickListener {
             Auth.signOut(this)
         }
     }
--- app/src/main/java/com/example/chitchatapp/firebase/Auth.kt
+++ app/src/main/java/com/example/chitchatapp/firebase/Auth.kt
@@ -5,6 +5,7 @@ import android.app.Activity.RESULT_OK
 import android.content.Intent
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.auth.FirebaseAuth
 
 class Auth {
@@ -23,11 +24,19 @@ class Auth {
         }
 
         fun signOut(activity: Activity) {
-            AuthUI.getInstance()
-                .signOut(activity)
-                .addOnCompleteListener {
-                    activity.finish()
+            MaterialAlertDialogBuilder(activity)
+                .setTitle("Sign Out")
+                .setMessage("Are you sure you want to sign out?")
+                .setPositiveButton("Yes") { _, _ ->
+                    AuthUI.getInstance()
+                        .signOut(activity)
+                        .addOnCompleteListener {
+                            activity.finish()
+                        }
                 }
+                .setNegativeButton("No") { _, _ -> }
+                .show()
+
         }
 
         fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
