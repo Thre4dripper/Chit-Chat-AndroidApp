056e465da4690cb87849880f0528c3ea87658697
Ijlal Ahmad <ijlalahmad845@gmail.com>
2025-03-17 00:33:10 +0530

    Refactored `initUserDetails` to remove the onSuccess callback, aligning with the absence of observer patterns in web implementations.

--- app/src/main/java/com/example/chitchatapp/viewModels/HomeViewModel.kt
+++ app/src/main/java/com/example/chitchatapp/viewModels/HomeViewModel.kt
@@ -75,13 +75,13 @@ class HomeViewModel(application: Application) : AndroidViewModel(application) {
         HomeRepository.checkCompleteRegistration {
             //init user details everytime even if the user is not completely registered
             //it will handle it inside the function
-            initUserDetails {
-                onSuccess(it)
-            }
+
+            // here the code differ from web implementation because in web there is no observer pattern like livedata
+            initUserDetails()
         }
     }
 
-    private fun initUserDetails(onSuccess: () -> Unit) {
+    private fun initUserDetails() {
         val context = getApplication<Application>().applicationContext
         val fcmToken = UserStore.getFCMToken(context)
         Log.d("FCM", "initUserDetails: $fcmToken")
@@ -89,7 +89,6 @@ class HomeViewModel(application: Application) : AndroidViewModel(application) {
         HomeRepository.getUsername { username ->
             //save username even if it is null
             UserStore.saveUsername(context, username)
-            onSuccess()
 
             //when username is null,then user details will be fetched from uid doc
             UserRepository.getUserDetails(context) {}
